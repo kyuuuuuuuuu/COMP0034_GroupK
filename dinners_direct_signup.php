bcdf4e3126f1395ed2b1ab8da7e205cc5e223350
Kien Nguyen <35876316+kyuuuuuuuuu@users.noreply.github.com>
2019-02-11 15:56:52 +0000

    Add html and css for Sign up page

--- dinners_direct_signup.php
+++ dinners_direct_signup.php
@@ -1,102 +1,73 @@
 <?php require_once ('dinners_direct_header.php');?>
 
     <div class="container">
-        <main>
-            <h3 class="headstyle"><b>Welcome to DinnersDirect!</b></h3><hr>
-            <h6 style="text-align:center">Let's set up your account. Already have one? Log in <a href="DinnersDirect_Login.html">here</a>.</h6><br>
+        <div class="card-header text-center">
+            <h1>Welcome to DinnersDirect</h1>
+        </div>
+        <div class="text-center">
+            <h2>
+                Let's set up your account. Already have one? Log in
+                <a href="dinners_direct_login.php">here.</a>
+            </h2>
+        </div>
+        <div class="col-lg-12 align-content-lg-center">
             <form>
-                <div class="row">
-                    <div class="col-md-3"></div>
-
-                    <div class="col-md-6">
-                        Email Address:
-                        <input class="border" name="Email Address" type="email">
+                <!--Check box to indicate type of account-->
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" value="" id="">
+                    <label class="form-check-label" for="defaultCheck1">School admin</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" value="" id="">
+                    <label class="form-check-label" for="defaultCheck1">Student</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" value="" id="">
+                    <label class="form-check-label" for="defaultCheck1">Parent</label>
+                </div>
+                <!--Sign up information required from user -->
+                <div class="form-group">
+                    <label>Email address</label>
+                    <input type="email" class="form-control" placeholder="Enter your email">
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Password">
                     </div>
-                    <div class="col-md-3"></div>
-                </div><br>
-
-                <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        First Name:
-                        <input class="border" name="First Name" type="text">
+                    <div class="form-group col-md-6">
+                        <label>Password confirmation</label>
+                        <input type="password" class="form-control" placeholder="Password confirmation">
                     </div>
-                    <div class="col-md-3"></div>
-                </div><br>
-
-                <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        Last Name:
-                        <input class="border" name="Last Name" type="text">
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label>First name</label>
+                        <input type="text" class="form-control" placeholder="First name">
                     </div>
-                    <div class="col-md-3"></div>
-                </div><br>
-
-                <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        Phone Number:
-                        <input class="border" name="Phone Number" type="tel">
+                    <div class="form-group col-md-6">
+                        <label>Last name</label>
+                        <input type="text" class="form-control" placeholder="Last name">
                     </div>
-                    <div class="col-md-3"></div>
-                </div><br>
-
-                <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        Password:
-                        <input class="border" name="Password" type="Password">
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label>Phone Number</label>
+                        <input type="number" class="form-control" placeholder="Phone number">
                     </div>
-                    <div class="col-md-3"></div>
-                </div><br>
-
-                <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        Confirm Password:
-                        <input class="border" name="Confirm Password" type="Password">
-                    </div>
-                    <div class="col-md-3"></div>
-                </div><br>
-
-
-                <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-4">
-                        Account Type:
-                        <label> <input name="Account Type" type="radio">Student</label>
-                    </div>
-                    <div class="col-md-1">
-                        <label><input name="Account Type" type="radio">Parent</label>
-                    </div>
-
-                    <div class="col-md-4"></div>
-                </div><br>
-
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="col-md-4">
-                        School:
-                        <select name="Schools">
-                            <option value="ucl">UCL</option>
-                            <option value="lse">LSE</option>
-                            <option value="Imperial">Imperial College London</option>
-                            <option value="Kings">Kings College London</option>
+                    <div class="form-group col-md-6">
+                        <label>School</label>
+                        <select class="form-control">
+                            <option>UCL</option>
+                            <option>LSE</option>
+                            <option>King's</option>
+                            <option>City</option>
+                            <option>QMUL</option>
                         </select>
                     </div>
-                    <div class="col-md-4"></div>
-                </div><br>
-
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="col-md-5">
-                        <button class="btn btn-secondary btn-block" type="Submit">Create My Account</button>
-                    </div>
-                    <div class="col-md-3"></div>
-                </div><br>
+                </div>
             </form>
-        </main>
+        </div>
     </div>
 
 <?php require_once ('dinners_direct_footer.php');?>
