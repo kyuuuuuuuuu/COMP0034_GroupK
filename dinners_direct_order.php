2050b70ba78b7ae7ed32efb91751279657744706
YeoJin <zcecyjl@ucl.ac.uk>
2019-02-12 08:45:18 +0000

    hi

--- dinners_direct_order.php
+++ dinners_direct_order.php
@@ -1,3 +1,7 @@
+<!--MAKE BOX AROUND EACH ITEM-->
+<!--MENU TO APPEAR AFTER CHOOSING DATE-->
+<!--*CHANGE* CHOOSE THE QUANTITY WHEN CLICKED-->
+
 <?php require_once ('dinners_direct_header.php');?>
 
     <div class="card-header text-center">
@@ -8,39 +12,41 @@
         <h3>Choose date</h3>
         <input type="date" id="orderDate" name="orderDate_">
         <br><br>
-<!--        MENU TO APPEAR AFTER CHOOSING DATE-->
         <hr>
         <br>
-        <p>Click items to choose</p>
+        <p>Click items to choose quantity</p>
         <div class="row">
         <div class="col-md-4 text-center">
         <h3>Main</h3>
             <div>
                 <img src="img/pasta.jpg" alt="Pasta" width="200" onclick="alert('Choose quantity')"/>
-                <br>Pasta</br>
+                <br>Pasta<br>
+                <b>2.50</b>/unit<br>
 <!--                *CHANGE* CHOOSE THE QUANTITY WHEN CLICKED-->
                 Choose the quantity: <input type="number" name="pastaQuantity" min="1" max="5">
-                <input type="submit" name="pastaQ" value="Go">
+                <button class="btn-secondary" type="submit">OK</button>
 <!--                put final one later??-->
             </div>
             <br>
 
             <div>
                 <img src="img/pizza.jpg" alt="Pizza" width="200"/>
-                <br>Pizza</br>
+                <br>Pizza<br>
+                <b>2.70/unit</b><br>
 <!--                CHANGE* CHOOSE THE QUANTITY WHEN CLICKED-->
                 Choose the quantity: <input type="number" name="pizzaQuantity" min="1" max="5">
-                <input type="submit" name="pizzaQ" value="Go">
+                <button class="btn-secondary" type="submit">OK</button>
 <!--                put final one later??-->
             </div>
             <br>
 
             <div>
-            <img src="img/lasagna.jpg" alt="lasagna" width="200"/>
-            <br>Lasagna</br>
+                <img src="img/lasagna.jpg" alt="lasagna" width="200"/>
+                <br>Lasagna<br>
+                <b>2.50/unit</b><br>
 <!--                CHANGE* CHOOSE THE QUANTITY WHEN CLICKED-->
                 Choose the quantity: <input type="number" name="lasagnaQuantity" min="1" max="5">
-                <input type="submit" name="lasagnaQ" value="Go">
+                <button class="btn-secondary" type="submit">OK</button>
 <!--                put final one later??-->
             </div>
         </div>
