c7ca95cbfec159d738fdf2ae0f4ea302f40b8be5
koonyewl <zceckyl@ucl.ac.uk>
2019-02-15 14:40:29 +0000

    after signup page

--- dinners_direct_student_MyAccount.php
+++ dinners_direct_student_MyAccount.php
@@ -7,7 +7,7 @@
     <div class="container">
         <br>
         <img class="img-fluid mb-5 d-block mx-auto rounded-circle" src="img/clubwelcome.jpg" alt="" style="width:600px;height:600px;">
-        <h1 class="text-uppercase mb-0">Thank You for signing up with DinnersDirect!</h1>
+        <h1 class="text-uppercase mb-0">Thank You for signing up with DinnersDirect!</h1>t
         <hr>
         <h2 class="font-weight-light mb-0">You will receive a confirmation email soon!</h2><br>
     </div>
