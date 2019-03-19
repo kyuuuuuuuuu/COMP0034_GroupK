163d34ac598d2e1695f4316615ce1dbbc1abf68a
Kien Nguyen <35876316+kyuuuuuuuuu@users.noreply.github.com>
2019-03-19 22:04:44 +0000

    change <a> tag to <button> in my account page

--- public/pages/myaccount_parent.php
+++ public/pages/myaccount_parent.php
@@ -45,7 +45,6 @@ require_once('../../private/shared/pages_header.php');
     <div class="row">
         <div class="col-md-1"></div>
         <div class="col-md-4">
-
             <img class="img-profile rounded-circle" src="../img/kien.jpg" style="width:200px;height:200px;"><br>
             <h2><?php echo $data['first_name'] . " " . $data['last_name'];?></h2>
             <!--            <div class="name"><small>Trung Kien Nguyen</small></div><br><br>-->
@@ -64,10 +63,10 @@ require_once('../../private/shared/pages_header.php');
         <div class="col-lg-4">
 
             <div class="nav list-group text-center text-uppercase">
-                <a class="nav-link list-group-item myaccount-nav" onclick="show_selected_tab(0)">Profile</a>
-                <a class="nav-link list-group-item myaccount-nav" onclick="show_selected_tab(1)">View Orders</a>
-                <a class="nav-link list-group-item myaccount-nav" onclick="show_selected_tab(2)">Edit Account</a>
-                <a class="nav-link list-group-item myaccount-logout" href="log_out.php">LOGOUT</a>
+                <button class="nav-link list-group-item myaccount-nav" onclick="show_selected_tab(0)">Profile</button>
+                <button class="nav-link list-group-item myaccount-nav" onclick="show_selected_tab(1)">View Orders</button>
+                <button class="nav-link list-group-item myaccount-nav" onclick="show_selected_tab(2)">Edit Account</button>
+                <button class="nav-link list-group-item myaccount-logout" href="log_out.php">LOGOUT</button>
             </div>
 
         </div>
@@ -121,12 +120,6 @@ require_once('../../private/shared/pages_header.php');
                     </div>
                 </div>
 
-
-
-
-
-
-
             </div>
         </div>
     </div>
@@ -135,47 +128,5 @@ require_once('../../private/shared/pages_header.php');
 </div>
 </body>
 
-
-
-<!---->
-<!--<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">-->
-<!--<div class="container-fluid">-->
-<!--    <div class="row navbar">-->
-<!--        <div class="col-md-9 card-header text-center">-->
-<!--            <h1>Welcome to My Account</h1>-->
-<!--        </div>-->
-<!--        <div class="col-md-3">-->
-<!--           <ul class="navbar-nav ml-auto align-content-center">-->
-<!--                <li class="nav-item dropdown no-arrow">-->
-<!--                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
-<!--                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Trung Kien Nguyen</span>-->
-<!--                        <img class="img-profile rounded-circle" src="img/kien.jpg" style="width:100px;height:100px;">-->
-<!--                    </a>-->
-<!--                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">-->
-<!--                        <a class="dropdown-item" href="#">-->
-<!--                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>-->
-<!--                            Profile-->
-<!--                        </a>-->
-<!--                        <a class="dropdown-item" href="#">-->
-<!--                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>-->
-<!--                            Settings-->
-<!--                        </a>-->
-<!--                        <a class="dropdown-item" href="#">-->
-<!--                            <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>-->
-<!--                            Activity Log-->
-<!--                        </a>-->
-<!--                        <div class="dropdown-divider"></div>-->
-<!--                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">-->
-<!--                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>-->
-<!--                            Logout-->
-<!--                        </a>-->
-<!--                    </div>-->
-<!--                </li>-->
-<!---->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
-<!--</nav>-->
 <?php require_once('../../private/shared/pages_footer.php');?>
 
