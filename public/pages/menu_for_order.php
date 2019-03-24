c2152e2acac8c8dbedee216a346630a0b2cf414c
Kyuuuuuuuuu <zcectkn@ucl.ac.uk>
2019-03-24 01:44:01 +0000

    yay, cookie for menu works

--- public/pages/menu_for_order.php
+++ public/pages/menu_for_order.php
@@ -1,25 +1,39 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['menu_id'])) {
     require_once($_SERVER['DOCUMENT_ROOT'] . "/COMP0034_GroupK/private/initialize.php");
- ?>
-
+    setcookie("chosen_menu", $_POST['menu_id']);
+}
+$list_menus = get_list_of_menus($db);
+if (isset($_COOKIE["chosen_menu"])) {
+    for ($j = 0; $j < count($list_menus); $j++) {
+        if ($_COOKIE["chosen_menu"] == $list_menus[$j]["menu_id"]) {
+            $cookie_menu_id = $_COOKIE["chosen_menu"];
+        }
+    }
+}
+$default_menu = $cookie_menu_id ?? $list_menus[0]["menu_id"];
+$menu_id = $_POST['menu_id'] ?? $default_menu;
+$item_in_menu = get_menu($db, $menu_id);
+$menu_info = get_data($db, $menu_id, 'list_of_menus', 'menu_id');
+?>
+    <h4 class="text-center"><strong><?= $menu_info["menu_name"]?></strong></h4>
 <div class="row">
 
+
     <?php
-    $menu_id = $_POST['menu_id'];
-    $menu = get_menu($db, $menu_id);
-    for ($i = 0; $i < count($menu); $i++) { if ($i == 0) {echo '<div class="col-md-4 text-center">
+
+    for ($i = 0; $i < count($item_in_menu); $i++) { if ($i == 0) {echo '<div class="col-md-4 text-center">
                     <br><h3>MAIN</h3>';}elseif ($i == 3) {echo '<div class="col-md-4 text-center">
                     <br><h3>DESSERTS</h3>';} elseif ($i == 6) {echo '<div class="col-md-4 text-center">
                     <br><h3>DRINK</h3>';}?>
         <div>
-            <img name="item_image" src="<?php echo $menu[$i]['item_image'];?>" alt="<?php echo $menu[$i]['item_name'];?>"
+            <img name="item_image" src="<?php echo $item_in_menu[$i]['item_image'];?>" alt="<?php echo $item_in_menu[$i]['item_name'];?>"
                  class="orderOutline<?php if ($i < 3) {echo "1";}elseif ($i<6) {echo "2";}else {echo "3";}?>"
                  data-toggle="modal" data-target="#Modal<?php echo $i;?>"/>
             <?php
-            $title= $menu[$i]['item_name'];
-            $price= $menu[$i]['item_price'];
-            $des= $menu[$i]['item_description'];
+            $title= $item_in_menu[$i]['item_name'];
+            $price= $item_in_menu[$i]['item_price'];
+            $des= $item_in_menu[$i]['item_description'];
             $modal_id = $i;?>
             <br>
             <div class="item_caption">
@@ -31,4 +45,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['menu_id'])) {
         <?php if($i == 2 || $i == 5 || $i == 8) {echo '</div>';}else {echo '<br>';} }?>
 </div>
 
-<?php }?>
+<?php ?>
--- public/pages/order.php
+++ public/pages/order.php
@@ -101,7 +101,8 @@ if ($not_log_in) {
                     <?php }?><br><br>
                 </div>
                 <div id="menu_set_la" class="col-md-9"> <!--This div is where the menu get printed by AJAX -->
-                    <p class="text-center">Please select a menu on the left-hand side.</p>
+<!--                    <h6 class="text-center">Please select a menu on the left-hand side.</h6>-->
+                    <?php require('menu_for_order.php');?>
                 </div>
             </div>
 
